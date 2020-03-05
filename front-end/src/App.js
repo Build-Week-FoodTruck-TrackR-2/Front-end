1ea72eacb88c717891c1dd55f1324431e57f647d
Brandon Franks <bgfranks106@icloud.com>
2020-03-05 17:30:25 -0500

    setting up switch routes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,22 +1,27 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 // import logo from './logo.svg';
-import Navigation from "./components/Navigation"
-import WelcomePage from "./components/WelcomePage";
-import DriverLogin from "./components/DriverLogin";
-import Register from "./components/Register"
-import FoodieLogin from "./components/FoodieLogin"
+import privateRoute from './components/privateRoute'
+import Navigation from './components/Navigation'
+import WelcomePage from './components/WelcomePage'
+import DriverLogin from './components/DriverLogin'
+import Register from './components/Register'
+import FoodieLogin from './components/FoodieLogin'
 
 function App() {
   return (
-    <main>
-      <Navigation />
-      <Route exact path ='/' component = {WelcomePage} />
-      <Route exact path ='/driver-login' component = {DriverLogin} />
-      <Route exact path ='/foodie-login' component = {FoodieLogin} />
-      <Route exact path ='/register' component = {Register} />
-    </main>
-  );
+    <Router>
+      <main>
+        <Navigation />
+        <Switch>
+          <Route exact path="/" component={WelcomePage} />
+          <Route exact path="/driver-login" component={DriverLogin} />
+          <Route exact path="/foodie-login" component={FoodieLogin} />
+          <Route exact path="/register" component={Register} />
+        </Switch>
+      </main>
+    </Router>
+  )
 }
 
-export default App;
+export default App
